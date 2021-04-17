400c9b27e9b179ff282f7c1e2e0818d3bba59083
Sook-Hee <sook-hee.evans@tufts.edu>
2021-04-17 15:40:29 -0400

    dict for all play types

--- sequence_dump.py
+++ sequence_dump.py
@@ -2,42 +2,45 @@ import operator
 import streamlit as st
 
 
-def run_analytics(games, team, sequence):
-    # print()
-    # print("in sequence dump")
-    # print()
+def run_analytics(games, team):
+    sequences = ["Spot-Up", "Transition", "Post-Up", "P&R Ball Handler", "Cut", "Hand Off", "Hand Off", "Offensive Rebounds",\
+        "Off Screen", "Isolation", "P&R Roll Man", "Miscellaneous"]
+    sequences=["ISO"]
     full_seq = True
     output = []
-    for game in games:
-        for poss in game:
-            if poss["team"] == team:
-                plays = poss["plays"]
-                
-                for index in range(0, len(plays)):
-                    match = True
+    plays_dict = {}
+    for seq in sequences:
+        for game in games:
+            for poss in game:
+                if poss["team"] == team:
+                    plays = poss["plays"]
+                    print("plays are", plays)
                     
-                    for seq_index in range(0, len(sequence)):
-                        if index + seq_index >= len(plays):
+                    for index in range(0, len(plays)):
+                        print(plays[index])
+                        match = True
+                        # index+1 is the play type, index is just the player
+                        if plays[index+1] != seq:
                             match = False
                             break
-                            
-                        if plays[index + seq_index] != sequence[seq_index]:
-                            match = False
-                            break
-                            
-                    if match:
-                        play_seq = ""
-                        if full_seq:
-                            for match_index in range(0, len(plays)):
-                                play_seq += "{} > ".format(plays[match_index])
-                            
-                            play_seq = play_seq[0:len(play_seq) - 2]
-                        else:
-                            for match_index in range(index, len(plays)):
-                                play_seq += "{} > ".format(plays[match_index])
-                            
-                            play_seq = play_seq[0:len(play_seq) - 2]
                         
-                        output.append(play_seq)
-                        break
-    return output
+                        if match:
+                            play_seq = ""
+                            if full_seq:
+                                for match_index in range(0, len(plays)):
+                                    play_seq += "{} > ".format(plays[match_index])
+                                
+                                play_seq = play_seq[0:len(play_seq) - 2]
+                            else:
+                                for match_index in range(index, len(plays)):
+                                    play_seq += "{} > ".format(plays[match_index])
+                                
+                                play_seq = play_seq[0:len(play_seq) - 2]
+                            
+                            output.append(play_seq)
+                            break
+                            
+        plays_dict[seq] = output
+    # print(plays_dict)
+    print(plays_dict["Cut"])
+    return plays_dict
