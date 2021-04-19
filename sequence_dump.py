516aad63d57defc42652facb9624c2cf16bb8ae8
Sook-Hee <sook-hee.evans@tufts.edu>
2021-04-19 18:15:47 -0400

    split plays on repeats

--- sequence_dump.py
+++ sequence_dump.py
@@ -9,8 +9,16 @@ def round_nums(x):
 
 def tally_stats(plays):
     tallies = {'attempts': 0, 'makes': 0, 'guarded': 0, 'open': 0, '3PT attempts': 0, '3PT makes': 0, 'turnovers': 0, 'possessions': 0, 'FT attempts': 0, 'FT makes': 0, 'points': 0}
+    
     for play in plays:
-        # print(play)
+        play_str = ""
+        if isinstance(play, list):
+            for poss in play:
+                # Counting repeats 
+                tallies['possessions'] += 1
+                play_str += " > " + poss
+            play = play_str
+
         seq = play.split(' > ')
         seq = [info.strip() for info in seq]
         for event in seq:
@@ -46,13 +54,14 @@ def compute_stats(tallies, game_count):
     stats = {}
     stats['poss/game'] = tallies['possessions'] / game_count
     stats['points'] = tallies['points'] / game_count
-    stats['PPP'] = tallies['points'] / tallies['possessions']
+
+    stats['PPP'] = tallies['points'] / tallies['possessions'] if tallies['possessions'] != 0 else "N/A"
     stats['FGM'] = tallies['makes'] / game_count
     stats['FGA'] = tallies['attempts'] / game_count
     stats['FG%'] = (stats['FGM'] / stats['FGA']) * 100 if stats['FGA'] != 0 else "N/A"
     # https://thesaucereport.wordpress.com/2009/04/28/adjusted-field-goal-percentage/
     stats['aFG%'] = ((stats['points'] - stats['FGM']) / (2 * stats['FGA'])) * 100 if stats['FGA'] != 0 else "N/A"
-    stats['%TO'] = (tallies['turnovers'] / tallies['possessions']) * 100
+    stats['%TO'] = (tallies['turnovers'] / tallies['possessions']) * 100 if tallies['possessions'] != 0 else "N/A"
     stats['%FT'] = (tallies['FT makes'] / tallies['FT attempts']) * 100 if tallies['FT attempts'] != 0 else "N/A"
     print(stats)
     return stats
@@ -60,19 +69,44 @@ def compute_stats(tallies, game_count):
 
 def run_analytics(games, team):
     sequences = ["Spot-Up", "Transition", "Post-Up", "P&R Ball Handler", "Cut", "Hand Off", "Offensive Rebounds", "Off Screen", "ISO", "P&R Roll Man", "Miscellaneous"]
-    sequences = ["Post-Up", "Spot-Up", "ISO", "P&R Ball Handler", "Hand Off", "Cut", "Transition", "Off Screen"]
-        
+    sequences = ["Spot-Up", "Transition", "Post-Up", "P&R Ball Handler", "Cut", "Hand Off","Off Screen", "ISO", "P&R Roll Man", "Miscellaneous"]
+
     full_seq = True
-    plays_dict = {}
+    plays_dict = {"Spot-Up": [], "Post-Up": [], "ISO": [], "P&R Ball Handler": [], "Hand Off": [], "Cut": [], "Transition": [], "Off Screen": [], "Offensive Rebounds": [], "P&R Roll Man": [], "Miscellaneous": []}
     for seq in sequences:
         output = []
         for game in games:
             for poss in game:
                 if poss["team"] == team:
                     plays = poss["plays"]
+                    
+                    repeat_indices = []
+                    repeat_output = ""
+                    for idx, play in enumerate(plays):
+                        if play in sequences and idx > 1:
+                            repeat_indices.append(idx)
+
+                    for idx, repeat in enumerate(repeat_indices):
+                        if idx < len(repeat_indices) - 1:
+                            repeat_output = ""
+                            for r in range(repeat_indices[idx], repeat_indices[idx + 1]):
+                                repeat_output += "{} > ".format(plays[r])
+                                
+                            repeat_output = repeat_output[0:len(repeat_output) - 2]
+                            plays_dict[plays[repeat]].append(repeat_output)
+
+                        else:
+                            repeat_output = ""
+                            for r in range(repeat_indices[idx], len(plays)):
+                                repeat_output += "{} > ".format(plays[r])
+                                
+                            repeat_output = repeat_output[0:len(repeat_output) - 2]
+                            plays_dict[plays[repeat]].append(repeat_output)
+
                     for index in range(len(plays)):
                         player = plays[0]
                         match = True
+                        
                         # index+1 is the play type, index is just the player
                         if plays[index+1] != seq:
                             match = False
@@ -93,9 +127,9 @@ def run_analytics(games, team):
                             
                             output.append(play_seq)
                             break
-                            
-        plays_dict[seq] = output
 
+        plays_dict[seq].append(output)      
+                   
     stat_dict = {}
     for seq in sequences:
         print(seq)
