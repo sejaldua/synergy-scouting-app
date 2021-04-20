8d12e7d6670ad076d569bfa934dd215e42874bd5
sejaldua <sejal.dua@tufts.edu>
2021-04-19 21:17:12 -0400

    refactor, convert N/A to NaN, and get rid of offensive rebound and misc

--- sequence_dump.py
+++ sequence_dump.py
@@ -1,73 +1,73 @@
 import pandas as pd
+import numpy as np
 
 def round_nums(x):
-    if x == "N/A":
+    try:
+        return round(x, 2)
+    except:
         return x
-    return round(x, 2)
+
+def parse_play(play_list, tallies):
+    seq = play_list.split(' > ')
+    seq = [info.strip() for info in seq]
+    for event in seq:
+        if event == 'Miss 2 Pts' or event == 'Miss 3 Pts':
+            tallies['attempts'] += 1
+        if event == 'Miss 3 Pts':
+            tallies['3PT attempts'] += 1
+        if event == 'Make 2 Pts' or event == 'Make 3 Pts':
+            tallies['makes'] += 1
+            tallies['attempts'] += 1
+            if event == 'Make 2 Pts':
+                tallies['points'] += 2
+            elif event == 'Make 3 Pts':
+                tallies['points'] += 3
+                tallies['3PT makes'] += 1
+                tallies['3PT attempts'] += 1
+        if event == 'Guarded':
+            tallies['guarded'] += 1
+        if event == 'Open':
+            tallies['open'] += 1
+        if event == 'Turnover':
+            tallies['turnovers'] += 1
+        if event == 'Free Throw':
+            tallies['FT attempts'] += 1
+        if event == 'Made':
+            tallies['points'] += 1
+            tallies['FT makes'] += 1
+    tallies['possessions'] += 1
+    return tallies
 
 def tally_stats(plays):
     tallies = {'attempts': 0, 'makes': 0, 'guarded': 0, 'open': 0, '3PT attempts': 0, '3PT makes': 0, 'turnovers': 0, 'possessions': 0, 'FT attempts': 0, 'FT makes': 0, 'points': 0}
     
     for play in plays:
-        play_str = ""
         if isinstance(play, list):
             for poss in play:
-                # Counting repeats 
-                tallies['possessions'] += 1
-                play_str += " > " + poss
-            print(play_str)
-            play = play_str
-
-        seq = play.split(' > ')
-        seq = [info.strip() for info in seq]
-        for event in seq:
-            if event == 'Miss 2 Pts' or event == 'Miss 3 Pts':
-                tallies['attempts'] += 1
-            if event == 'Miss 3 Pts':
-                tallies['3PT attempts'] += 1
-            if event == 'Make 2 Pts' or event == 'Make 3 Pts':
-                tallies['makes'] += 1
-                tallies['attempts'] += 1
-                if event == 'Make 2 Pts':
-                    tallies['points'] += 2
-                elif event == 'Make 3 Pts':
-                    tallies['points'] += 3
-                    tallies['3PT makes'] += 1
-                    tallies['3PT attempts'] += 1
-            if event == 'Guarded':
-                tallies['guarded'] += 1
-            if event == 'Open':
-                tallies['open'] += 1
-            if event == 'Turnover':
-                tallies['turnovers'] += 1
-            if event == 'Free Throw':
-                tallies['FT attempts'] += 1
-            if event == 'Made':
-                tallies['points'] += 1
-                tallies['FT makes'] += 1
-        tallies['possessions'] += 1
-    print(tallies)
+                tallies = parse_play(poss, tallies)
+        else:
+            tallies = parse_play(play, tallies)
     return tallies
 
 def compute_stats(tallies, game_count):
     stats = {}
     stats['poss/game'] = tallies['possessions'] / game_count
     stats['points'] = tallies['points'] / game_count
-
-    stats['PPP'] = tallies['points'] / tallies['possessions'] if tallies['possessions'] != 0 else "N/A"
+    stats['PPP'] = tallies['points'] / tallies['possessions'] if tallies['possessions'] != 0 else np.nan
     stats['FGM'] = tallies['makes'] / game_count
     stats['FGA'] = tallies['attempts'] / game_count
-    stats['FG%'] = (stats['FGM'] / stats['FGA']) * 100 if stats['FGA'] != 0 else "N/A"
+    stats['FG%'] = (stats['FGM'] / stats['FGA']) * 100 if stats['FGA'] != 0 else np.nan
     # https://thesaucereport.wordpress.com/2009/04/28/adjusted-field-goal-percentage/
-    stats['aFG%'] = ((stats['points'] - stats['FGM']) / (2 * stats['FGA'])) * 100 if stats['FGA'] != 0 else "N/A"
-    stats['%TO'] = (tallies['turnovers'] / tallies['possessions']) * 100 if tallies['possessions'] != 0 else "N/A"
-    stats['%FT'] = (tallies['FT makes'] / tallies['FT attempts']) * 100 if tallies['FT attempts'] != 0 else "N/A"
+    stats['aFG%'] = ((stats['points'] - stats['FGM']) / (2 * stats['FGA'])) * 100 if stats['FGA'] != 0 else np.nan
+    stats['%TO'] = (tallies['turnovers'] / tallies['possessions']) * 100 if tallies['possessions'] != 0 else np.nan
+    stats['%FT'] = (tallies['FT makes'] / tallies['FT attempts']) * 100 if tallies['FT attempts'] != 0 else np.nan
     print(stats)
     return stats
 
 
 def run_analytics(games, team):
     sequences = ["Spot-Up", "Transition", "Post-Up", "P&R Ball Handler", "Cut", "Hand Off", "Offensive Rebounds", "Off Screen", "ISO", "P&R Roll Man", "Miscellaneous"]
+    sequences = ["Spot-Up", "Transition", "Post-Up", "P&R Ball Handler", "Cut", "Hand Off", "Off Screen", "ISO", "P&R Roll Man"]
 
     plays_dict = {seq: [] for seq in sequences}
     full_seq = True
